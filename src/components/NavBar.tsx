20395fd7d9f313276cdec016a4f2956af4cc7eb3
20220005 <20220005@itla.edu.do>
2024-03-06 11:52:09 -0400

    Corrigiendo nombre en Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ function NavBar() {
    <>
    <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid ">
-    <Link to="/" className="navbar-brand">EdyAsyncHub</Link>
+    <Link to="/" className="navbar-brand">EduAsyncHub</Link>
    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
