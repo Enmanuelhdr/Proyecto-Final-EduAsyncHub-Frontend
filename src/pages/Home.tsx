0171845e16060628b8d3784752404ef6d2bf25fe
20220005 <20220005@itla.edu.do>
2024-03-07 16:47:49 -0400

    Apartado de eventos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,19 @@
 import NavBar from "../components/NavBar";
 import Carousel from "../components/Carousel";
-
+import Eventos from "../components/Evento";
 
 function Home() {
- 
-
-
- 
   return (
     <>
-    <div className="">
       <NavBar />
-    </div>
+      <div className="pt-3">
+        <Carousel />
+      </div>
 
-
-    <div className="pt-3">
-      <Carousel/>
-    </div>
-
-   
+      <div className=" pt-5 d-flex flex-column justify-content-center align-items-center gap-3 pb-5">
+        <h3>Eventos</h3>
+        <Eventos cantidadMostrar={3} />
+      </div>
     </>
   );
 }
