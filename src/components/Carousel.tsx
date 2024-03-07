fb357cc20a034e012c21dd3cff289e7c687d4bf5
20220005 <20220005@itla.edu.do>
2024-03-07 19:39:21 -0400

    Carousel auto

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,7 @@ import Carouseldata from "../data/Carousel.json";
 function Carousel() {
   return (
     <>
-    <div id="carouselExample" className=" container carousel slide">
+    <div id="carouselExampleAutoplaying" className=" container carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner ">
 
 
@@ -22,30 +22,14 @@ function Carousel() {
             })}
                
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
       </div>
 
     </>
