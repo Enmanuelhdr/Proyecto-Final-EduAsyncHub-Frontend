e514e312e092c9bb7ffd0596d63c87e52733b305
20220005 <20220005@itla.edu.do>
2024-03-13 13:30:34 -0400

    fix carousel bug

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,37 +1,40 @@
+import { useState } from 'react';
 import Carouseldata from "../data/Carousel.json";
 
 function Carousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const nextSlide = () => {
+    const lastIndex = Carouseldata.length - 1;
+    const newIndex = activeIndex === lastIndex ? 0 : activeIndex + 1;
+    setActiveIndex(newIndex);
+  };
+
+  const prevSlide = () => {
+    const lastIndex = Carouseldata.length - 1;
+    const newIndex = activeIndex === 0 ? lastIndex : activeIndex - 1;
+    setActiveIndex(newIndex);
+  };
+
   return (
     <>
-    <div id="carouselExampleAutoplaying" className=" container carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner ">
-
-
-
-            {Carouseldata.map((data) => {
-              return (
-                <div className="carousel-item h-500 active" key={data.id}>
-                  <img
-                    src={data.img}
-                    alt={data.name}
-                    className="d-block w-100 "/>
-                    
-                
-                </div>
-              );
-            })}
-               
+      <div id="carouselExampleAutoplaying" className="container carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          {Carouseldata.map((data, index) => (
+            <div className={`carousel-item ${index === activeIndex ? 'active' : ''}`} key={data.id}>
+              <img src={data.img} alt={data.name} className="d-block w-100" />
+            </div>
+          ))}
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev" onClick={prevSlide}>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next" onClick={nextSlide}>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-
     </>
   );
 }
