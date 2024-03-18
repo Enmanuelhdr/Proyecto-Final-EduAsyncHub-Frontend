8a3118f5ee9ce0b01a0e387c5a93fb4736131311
20220005 <20220005@itla.edu.do>
2024-03-18 11:17:15 -0400

    Cambiando estilos de las card noticias

--- src/components/Noticia.tsx
+++ src/components/Noticia.tsx
@@ -10,25 +10,30 @@ const Noticia: React.FC<Props> = ({ cantidadMostrar }) => {
   return (
     <>
       <div className="container-fluid">
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-3 g-4">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 d-flex justify-content-center align-items-center">
           {noticias.slice(0, cantidadMostrar).map((noticia) => (
             <div
               className="col"
+              style={{
+                height: "300px",
+               
+             
+              }}
               key={noticia.id}
               onClick={() => {
                 console.log(noticia.id);
               }}
             >
-              <div className="card h-100">
+              <div className="bg-image rounded-4 h-100 hover-zoom">
                 <img
-                  className="card-img-top"
+                  className="w-100 h-100 fill object-fit-cover "
                   src={noticia.img}
                   alt="Imagen del noticia"
                 />
-                <div className="card-body">
-                  <div className="fs-6">{noticia.date}</div>
-                  <h4 className="card-title">{noticia.title}</h4>
-                  <p className="card-text">{noticia.description}</p>
+                <div className="mask d-flex flex-column h-100 align-items-center text-center justify-content-center p-5 pb-3 " style={{backgroundColor: 'hsla(0, 0%, 0%, 0.6)'}}>
+                  <div className="fs-6 text-white overflow-hidden">{noticia.date}</div>
+                  <h4 className="fw-bold text-white mb-4">{noticia.title}</h4>
+                  <p className="text-white overflow-hidden">{noticia.description}</p>
                 </div>
               </div>
             </div>
