eaa4b45f43f18c682c47d3c39fd672f56c713010
20220005 <20220005@itla.edu.do>
2024-04-10 15:03:22 -0400

    Agregando la data del navbar de estudiante

--- src/pages/userViews/viewStudent/DashboardStudent.tsx
+++ src/pages/userViews/viewStudent/DashboardStudent.tsx
@@ -1,11 +1,19 @@
 import MiCalificacion from "./MiCalificacion"
-
+import navData from "../../../data/EstudianteNavbar.json";
+import NavBar from "../../../components/NavBar";
 
 function DashboardStudent() {
   return (
     <>
-   
+   <NavBar
+        brand="Panel Estudiante"
+        goto="/dashboardprofesor"
+        navData={navData}
+      />
+      <div className="mt-4">
+
   <MiCalificacion />
+      </div>
     </>
   )
 }
